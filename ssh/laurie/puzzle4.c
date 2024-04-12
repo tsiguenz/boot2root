0bb5ae1335a22167734473ca23e5ca4f0ed6ff69
User <parrot@localhost.localdomain>
2024-04-12 11:01:50 +0100

    Add readme.md for each services

--- ssh/laurie/puzzle4.c
+++ ssh/laurie/puzzle4.c
@@ -10,9 +10,7 @@ int func4(int param_1)
   }
   else {
     iVar1 = func4(param_1 + -1);
-printf("iVar1 %d %d\n", param_1,  iVar1);
     iVar2 = func4(param_1 + -2);
-printf("Ivar2 %d %d\n", param_1, iVar2);
     iVar2 = iVar2 + iVar1;
   }
   return iVar2;
@@ -22,15 +20,15 @@ printf("Ivar2 %d %d\n", param_1, iVar2);
 
 int main()
 {
-    int i =0;
+    int i = 10;
+    int ret = 0;
     while(i < 100) {
         if (func4(i) == 55) {
             printf("%d\n", i);
             break;
         }
+        printf("%d\n", i);
         i++;
-    }
-    printf("Hello World %d\n", func4(-1));
-    
+    }    
     return 0;
 }
