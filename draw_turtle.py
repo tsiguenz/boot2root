dc300288bcb9eafb45a2fc28e01443d16b72004f
tsiguenz <tsiguenz@student.42.fr>
2024-04-13 23:51:35 +0200

    add draw turtle for zaz password

---
+++ draw_turtle.py
@@ -0,0 +1,22 @@
+import turtle as t
+
+if __name__ == "__main__":
+    with open("turtle", "r") as file:
+        fileContent = file.read()
+    lines = fileContent.split("\n")
+    i = 0
+    for line in lines:
+        words = line.split()
+        if len(words) == 0:
+            continue
+        match words[0]:
+            case "Avance":
+                t.forward(int(words[1]))
+            case "Recule":
+                t.backward(int(words[1]))
+            case "Tourne":
+                if words[1] == "droite":
+                    t.right(int(words[3]))
+                if words[1] == "gauche":
+                    t.left(int(words[3]))
+    t.exitonclick()
